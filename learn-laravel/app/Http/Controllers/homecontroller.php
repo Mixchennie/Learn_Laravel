13416231f68fc1422aecdd3667fc334fe0d76054
Mixchennie <nhung.phan25@student.passerellesnumeriques.org>
2024-02-28 07:38:41 +0700

    Laravel-Bai12

--- learn-laravel/app/Http/Controllers/homecontroller.php
+++ learn-laravel/app/Http/Controllers/homecontroller.php
@@ -19,7 +19,8 @@ class HomeController extends Controller
         $this->data['index'] = 1;
 
         $this->data['dataArr'] = [];
-        $this->data['number'] = 20;
+        $this->data['number'] = 3;
+        $this->data['message'] = 'Đặt hàng thành công';
         return view('home', $this->data);
     }
 }
