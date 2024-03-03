1140da0413c565875e50e7f7e81371a0e54559a2
Mixchennie <nhung.phan25@student.passerellesnumeriques.org>
2024-03-03 15:57:45 +0700

    Laravel-bai19

--- learn-laravel/routes/web.php
+++ learn-laravel/routes/web.php
@@ -21,29 +21,15 @@ Route::get('/san-pham', [HomeController::class, 'products'])->name('product');
 Route::get('/them-san-pham', [HomeController::class, 'getAdd']);
 // Route::post('/them-san-pham', [HomeController::class, 'postAdd']);
 Route::put('/them-san-pham', [HomeController::class, 'putAdd']);
-
-Route::get('demo-response', function () {
-    $contentArr = [
-        'name' => 'Laravel 8.x',
-        'lesson' => 'Khóa học lập trình Laravel',
-        'academy' => 'Unicode Academy'
-    ];
-    return $contentArr;
-});
-
 Route::get('lay-thong-tin', [HomeController::class, 'getArr']);
 
 Route::get('demo-response', function () { 
-    // return view('clients.contents.demo-test');
-    $response = response()
-    ->view('clients.contents.demo-test',[
-        'title' => 'Học Http response',
-    ], 201)
-    ->header('Content-Type', 'application/json')
-    ->header('API-key', '123456');
-    return $response;
-});
+    return view('clients.contents.demo-test');
+})->name('demo-response');
 
-// Route::get('demo-response-2', function (Request $request){
-//     return $request->cookie('unicode');
-// });
+Route::post('demo-response', function(Request $request){
+    if(!empty($request->username)){
+        return back()->withInput()->with('mess', 'validate thành công');
+    }
+    return redirect(route('demo-response'))->with('mess', 'Validate không thành công');
+});
