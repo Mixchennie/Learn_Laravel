3192b296a2349d7f50fef0ce9570547fa73098be
Mixchennie <143242265+Mixchennie@users.noreply.github.com>
2024-03-10 17:54:45 +0700

    Laravel-Bai24 (#30)

--- learn-laravel/routes/web.php
+++ learn-laravel/routes/web.php
@@ -19,21 +19,21 @@ use Illuminate\Http\Response;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/san-pham', [HomeController::class, 'products'])->name('product');
 Route::get('/them-san-pham', [HomeController::class, 'getAdd']);
-// Route::post('/them-san-pham', [HomeController::class, 'postAdd']);
+Route::post('/them-san-pham', [HomeController::class, 'postAdd']);
 Route::put('/them-san-pham', [HomeController::class, 'putAdd']);
 Route::get('/lay-thong-tin', [HomeController::class, 'getArr']);
 
-// Route::get('demo-response', function () { 
-//     return view('clients.contents.demo-test');
-// })->name('demo-response');
+Route::get('demo-response', function () { 
+    return view('clients.contents.demo-test');
+})->name('demo-response');
 
-// Route::post('demo-response', function(Request $request){
-//     if(!empty($request->username)){
-//         return back()->withInput()->with('mess', 'validate thành công');
-//     }
-//     return redirect(route('demo-response'))->with('mess', 'Validate không thành công');
-// });
+Route::post('demo-response', function(Request $request){
+    if(!empty($request->username)){
+        return back()->withInput()->with('mess', 'validate thành công');
+    }
+    return redirect(route('demo-response'))->with('mess', 'Validate không thành công');
+});
 
-// Route::get('download-image', [HomeController::class, 'downloadImage'])->name('download-image');
-// Route::get('download-doc', [HomeController::class, 'downloadImage'])->name('download-image');
+Route::get('download-image', [HomeController::class, 'downloadImage'])->name('download-image');
+Route::get('download-doc', [HomeController::class, 'downloadImage'])->name('download-image');
 
